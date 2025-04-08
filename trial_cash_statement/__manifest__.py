f60cf02166906e4021d2accd8a03924609b810ff
MOSHUKUR <mahmadoshokr@gmail.com>
2025-04-08 22:36:09 +0200

    [TRIAL]

---
+++ trial_cash_statement/__manifest__.py
@@ -0,0 +1,13 @@
+#
+{
+    "name": "TRIAL Cash Statement Extension",
+    "summary": "Update cash balance",
+    "version": "16.0.3.0.0",
+    "author": "TRIAL USAGE",
+    "license": "OPL-1",
+    "website": "TRIAL USAGE",
+    "category": "Accounting",
+    "depends": ["account"],
+    "data": ["wizard/account_cash_update_balances_view.xml", "security/ir.model.access.csv"],
+    "images": ["static/description/main_screenshot.png"],
+}
